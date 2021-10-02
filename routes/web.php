f5e155f9482a9896b93713b468c4ea368f681564
darman1725 <darmansaragih1701@gmail.com>
2021-10-02 15:29:28 +0700

    Menambahkan Route Setiap Pages

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,48 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* File Pada Halaman About Beauty Care */
+Route::get('/about', function () {
+    return view('about');
+});
+
+/* File Pada Halaman Blog Single Beauty Care */
+Route::get('/blog-single', function () {
+    return view('blog-single');
+});
+
+/* File Pada Halaman Blog Single Beauty Care */
+Route::get('/blog', function () {
+    return view('blog');
+});
+       
+/* File Pada Halaman Contact Beauty Care */
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+/* File Pada Halaman Index Beauty Care */
+Route::get('/index', function () {
+    return view('index');
+});
+
+/* File Pada Halaman Main Beauty Care */
+Route::get('/main', function () {
+    return view('main');
+});
+
+/* File Pada Halaman Pricing Beauty Care */
+Route::get('/pricing', function () {
+    return view('pricing');
+});
+
+/* File Pada Halaman Specialist Beauty Care */
+Route::get('/specialist', function () {
+    return view('specialist');
+});
+
+/* File Pada Halaman Treatments Beauty Care */
+Route::get('/treatments', function () {
+    return view('treatments');
+});
