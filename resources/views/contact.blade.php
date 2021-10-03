a4274eead5e68c33e738ccef07350b97219336c7
darman1725 <darmansaragih1701@gmail.com>
2021-10-03 07:44:44 +0700

    Menambahkan Informasi Halaman Kontak

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -67,19 +67,19 @@
 					<div class="col-md-4 contact-info ftco-animate bg-light p-4">
 						<div class="row">
 							<div class="col-md-12 mb-4">
-	              <h2 class="h4">Contact Information</h2>
+	              <h2 class="h4">Informasi Kontak</h2>
 	            </div>
 	            <div class="col-md-12 mb-3">
-	              <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
+	              <p><span>Alamat:</span> Jl. Sukarno Hatta No 32, Malang</p>
 	            </div>
 	            <div class="col-md-12 mb-3">
-	              <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
+	              <p><span>Telephone:</span> <a href="tel://1234567920">081375685166</a></p>
 	            </div>
 	            <div class="col-md-12 mb-3">
-	              <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+	              <p><span>Email:</span> <a href="mailto:info@yoursite.com">darmansaragih087@gmail.com</a></p>
 	            </div>
 	            <div class="col-md-12 mb-3">
-	              <p><span>Website:</span> <a href="#">yoursite.com</a></p>
+	              <p><span>Website:</span> <a href="{{ url('index') }}">yoursite.com</a></p>
 	            </div>
 						</div>
 					</div>
@@ -89,12 +89,12 @@
             	<div class="row">
             		<div class="col-md-6">
 	                <div class="form-group">
-	                  <input type="text" class="form-control" placeholder="Your Name">
+	                  <input type="text" class="form-control" placeholder="Nama Anda">
 	                </div>
                 </div>
                 <div class="col-md-6">
 	                <div class="form-group">
-	                  <input type="text" class="form-control" placeholder="Your Email">
+	                  <input type="text" class="form-control" placeholder="Email Anda">
 	                </div>
 	                </div>
               </div>
@@ -102,10 +102,10 @@
                 <input type="text" class="form-control" placeholder="Subject">
               </div>
               <div class="form-group">
-                <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+                <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Pesan"></textarea>
               </div>
               <div class="form-group">
-                <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
+                <input type="submit" value="Kirim Pesan" class="btn btn-primary py-3 px-5">
               </div>
             </form>
           </div>
