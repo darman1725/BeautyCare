a6cd22e3a47f0dd5964d53192877246f84e9b66b
darman1725 <darmansaragih1701@gmail.com>
2021-10-07 14:53:23 +0700

    Perbaiki File web.php

---
+++ routes/web.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/acne', function () {
+    return view('acne');
+});
+
+Route::get('/botox', function () {
+    return view('botox');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/electrocautery', function () {
+    return view('electrocautery');
+});
+
+Route::get('/glowarmpit', function () {
+    return view('glowarmpit');
+});
+
+Route::get('/glowneck', function () {
+    return view('glowneck');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/purifying', function () {
+    return view('purifying');
+});
+
+Route::get('/specialists', function () {
+    return view('specialists');
+});
+
+Route::get('/treatments', function () {
+    return view('treatments');
+});
+
+
+
