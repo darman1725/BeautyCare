2419681e53d4a66b21b565285b91dd2fc828900c
darman1725 <darmansaragih1701@gmail.com>
2021-10-07 13:52:23 +0700

    Penyesuaian Folder

--- app/Http/Controllers/BeautyCareController.php
+++ app/Http/Controllers/BeautyCareController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class BeautyCareController extends Controller
 {
-    //
+    public function index()
+    {
+        $user = Auth::user();
+        return view('menu.index',['user' => $user]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,83 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function () {
-    return view('home');
-});
-
-/* File Pada Halaman About Beauty Care */
-Route::get('/sigin', function () {
-    return view('login');
-});
-
-/* File Pada Halaman About Beauty Care */
-Route::get('/about', function () {
-    return view('about');
-});
-
-/* File Pada Halaman Contact Beauty Care */
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-/* File Pada Halaman Index Beauty Care */
-Route::get('/index', function () {
-    return view('index');
-});
 
 
-/* File Pada Halaman Main Beauty Care */
-Route::get('/main', function () {
-    return view('main');
-});
-
-/* File Pada Halaman Specialist Beauty Care */
-Route::get('/specialists', function () {
-    return view('specialists');
-});
-
-/* File Pada Halaman Treatments Beauty Care */
-Route::get('/treatments', function () {
-    return view('treatments');
-});
-
-/* File Pada Halaman Treatments Beauty Care */
-Route::get('/acne', function () {
-    return view('acne');
-});
-
-/* File Pada Halaman Treatments Beauty Care */
-Route::get('/botox', function () {
-    return view('botox');
-});
-
-/* File Pada Halaman Treatments Beauty Care */
-Route::get('/electrocautery', function () {
-    return view('electrocautery');
-});
-
-/* File Pada Halaman Treatments Beauty Care */
-Route::get('/glowarmpit', function () {
-    return view('glowarmpit');
-});
-
-/* File Pada Halaman Treatments Beauty Care */
-Route::get('/glowneck', function () {
-    return view('glowneck');
-});
-
-/* File Pada Halaman Treatments Beauty Care */
-Route::get('/purifying', function () {
-    return view('purifying');
-});
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard.index');
-});
-
-Auth::routes();
+/* Folder Menu */
+Route::get('/', [BeautyCareController::class, 'index'])->name('menu.index');
