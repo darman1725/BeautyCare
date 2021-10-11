b3a02a0bb028c3670d587613bb73b29a2794f442
MutiaraRahmanda <mutiararizkia18@gmail.com>
2021-10-11 20:02:50 +0700

    Modifkasi Halaman Treatments

--- resources/views/customers/customersTreatments.blade.php
+++ resources/views/customers/customersTreatments.blade.php
@@ -2,36 +2,49 @@
 
 @section('content')
 
-<section class="treatments spad">
+<section class="ftco-section ftco-no-pt ftco-no-pb">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-9">
-                <div class="row">
-                    @foreach ($treatments as $treatment)
-                        <div class="col-lg-4 col-md-4">
-                            <div class="treatment__item">
-                                <div class="treatment__item__pic__slider owl-carousel">
-                                    <img src="{{ asset('storage/' . $treatment->image) }}" alt="" style="width: 262px" height="151px">
-                                </div>
-                                <div class="treatment__item__text">
-                                    <div class="treatment__item__text__inner">
-                                        <h5 style="height: 70px !important"><a href="{{ route('detail', $treatment->id) }}">{{ $treatment->nama }}</a></h5>
-
-                                        <span>{{ $treatment->deskripsi}}</span>
-
-                                    </div>
-                                    <div class="treatment__item__price">
-                                        <a href="{{ route('treatments.create', $treatment->id) }}"><span class="treatments-option">Detail</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
+        <div class="row no-gutters">
+            @foreach ($treatments as $treatment)
+            <div class="col-md-4 d-flex align-items-stretch">
+                <div class="offer-deal text-center px-2 px-lg-5" style="width:400px">
+                    <div class="card-body">
+                        <img src="{{ asset('images/' . $treatment->image) }}" alt="Card Image" style="width: 262px" height="151px">
+                        <br><br>
+                        <h4 class="mb-4" align="center">{{$treatment->nama}}</h4>
+                        <hr />
+                        <p class="mb-5">{{$treatment->deskripsi}}</p>
+                        <br>
+                        <a href="{{ route('treatments.detail', $treatment->id) }}"><img src="assets/images/shopping-cart.png" style="width:8%">Detail</a>
+                        {{-- <p><a href="electrocautery" class="btn btn-white px-4 py-3"><img src="assets/images/shopping-cart.png" style="width:8%"> Detail</span></a></p> --}}
+                    </div>
                 </div>
-                {{ $treatments->links() }}
             </div>
         </div>
     </div>
 </section>
-<!-- Car Section End -->
+
+
+<section class="ftco-section">
+    <div class="container">
+        <div class="row block-9">
+            <div class="col-md-4 contact-info ftco-animate">
+                <div class="row">
+                    <img src="{{ asset('images/' . $treatment->image) }}" alt="" style="width: 262px" height="151px">
+                </div>
+                <br>
+                <p class="mb-4" align="center">Price</p>
+                <h2>{{$treatment->harga}}</h2>
+                <hr />
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-6 ftco-animate">
+                <h2>{{$treatment->nama}}</h2>
+                <p>{{$treatment->deskripsi}}</p>
+                <input type="submit" value="Reservasi" class="btn btn-primary py-3 px-5">
+            </div>
+        </div>
+    </div>
+</section>
+
 @endsection
--- resources/views/layouts/treatments.blade.php
+++ resources/views/layouts/treatments.blade.php
@@ -49,6 +49,17 @@
 	  </nav>
     <!-- END nav -->
 
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('assets/images/bg_2.jpg');" data-stellar-background-ratio="0.5">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
+            <div class="col-md-9 ftco-animate text-center">
+              <h1 class="mb-3 bread">Treatments</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+
     <!-- Content -->
     @yield('content')
     <!-- Content End -->
